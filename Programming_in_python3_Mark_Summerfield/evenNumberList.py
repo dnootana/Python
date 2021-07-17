7e36d289e5fa1f163204a8501d3c3a7774fd7997
dnootana <dnootana27@gmail.com>
2021-07-17 20:18:46 +0530

    some test files added

---
+++ Programming_in_python3_Mark_Summerfield/evenNumberList.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+list = input("Enter numbers : ")
+L = list.split()
+E = []
+for i in range(len(L)):
+    num = int(L[i],10)
+    if(num%2==0):
+        E.append(L[i])
+print("Even numbers are : ",E)
