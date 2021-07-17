26e5b599423a24e049b4da5fcaec99f729398ad7
dnootana <dnootana27@gmail.com>
2021-07-17 16:55:17 +0530

    small change

--- matrix.py
+++ matrix.py
@@ -1,15 +1,20 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-r = input("Enter no of matrix row")
+r = int(input("Enter no of matrix rows : "))
 #c = input("Enter no of matrix column")
 A = []
-for i in range(int(r)):
+max = 0
+for i in range(r):
+    a = []
     row_list = input("Enter row "+str(i+1)+" columns : ")
-    A.append(row_list.split())
-    max = len(A[i])
+    a=list(map(int,row_list.split()))
+    A.append(a)
+    if len(A[i])>max:
+        max = len(A[i])
 
-print(A)
-for i in range(int(r)):
-    D = [0 for i in range(max)]
-    D[0:len(A(i))] = A[i]
+print('Original Matrix : ',A)
+print('Maximum columns : ',max)
+for i in range(r):
+    D = [0 for k in range(max)]
+    D[0:len(A[i])] = A[i]
     A[i] = D
-print(A)
+print('Final Matrix : ',A)
