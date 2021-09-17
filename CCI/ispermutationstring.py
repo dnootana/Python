b7e4015a74fca9e69e9de263f581a7544d839e46
dnootana <dnootana27@gmail.com>
2021-09-17 17:14:20 +0530

    locating the permuted string in main string

--- CCI/ispermutationstring.py
+++ CCI/ispermutationstring.py
@@ -15,6 +15,42 @@ def isPermutationString(Str,Substr):
 				break
 	return location,count
 
-locations,count = isPermutationString('cbabadcbbabbcbabaabccbabc','abbc')
+def isPermutationString1(Str,Substr):
+	count = 0
+	location = []
+	sorted_substr = sorted(Substr)
+	for i in range(len(Str)-len(Substr)+1):
+		if sorted(Str[i:i+len(Substr)]) == sorted_substr:
+			location.append(i)
+			count += 1
+	return location,count
+
+def isPermutationString2(Str,Substr):
+	count = 0
+	location = []
+	max = 256
+
+	for num in range(len(Str)-len(Substr)+1):
+		hcount = [0]*max
+		compStr = Str[num:num+len(Substr)]
+		for i in range(len(Str)):
+			hcount[compStr[i]] += 1
+			hcount[Substr[i]] -= 1
+
+		if all(val==0 for val in hcount):
+			location.append(i)
+			count += 1
+	return location,count
+
+mainString = 'cbabadcbbabbcbabaabccbabc'
+subString = 'abbc'
+locations,count = isPermutationString(mainString,subString)
+print(locations)
+print(count)
+locations,count = isPermutationString1(mainString,subString)
+print(locations)
+print(count)
+
+locations,count = isPermutationString1(mainString,subString)
 print(locations)
 print(count)
