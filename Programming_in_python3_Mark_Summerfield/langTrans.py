494459fb5a29fad304ded35d7a549bec54fc3f28
dnootana <dnootana27@gmail.com>
2021-11-20 20:42:20 +0530

    files deleted

--- Programming_in_python3_Mark_Summerfield/langTrans.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3.8
-
-table="".maketrans("0123456789","\N{Devanagari digit zero}\N{Devanagari digit one}"
-"\N{Devanagari digit two}\N{Devanagari digit three}"
-"\N{Devanagari digit four}\N{Devanagari digit five}"
-"\N{Devanagari digit six}\N{Devanagari digit seven}"
-"\N{Devanagari digit eight}\N{Devanagari digit nine}")
-print("0123456789".translate(table))
