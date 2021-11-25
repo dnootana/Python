bb3de48bbba6229b15b8a89494fc7440fa58ba93
dnootana <dnootana27@gmail.com>
2021-11-25 11:54:37 +0530

    code reviewed

--- Interview/hackerrank/fibonacci_generator.py
+++ Interview/hackerrank/fibonacci_generator.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3.8
+
+number = 10
 def fib_gen(up_to):
+	"""
+		fibonacci series upto number 50 (i.e. <=50)
+	"""
 	a,b=0,1
 	while a<up_to:
 		yield a
 		a,b=b,a+b
 
 fib_num = []
-for num in fib_gen(50):
+for num in fib_gen(number):
 	fib_num.append(num)
 print('Fibonacci series upto 50 are : ',', '.join(list(map(str,fib_num))))
 
 def fib_gen1():
+	"""
+		fibonacci series upto 50 numbers
+	"""
 	a,b=0,1
 	while 1:
 		yield a
@@ -18,6 +26,6 @@ def fib_gen1():
 
 fib_num1 = []
 x = fib_gen1()
-for num in range(50):
+for num in range(number):
 	fib_num1.append(x.__next__())
 print('Fibonacci series of 50 numbers are : ',', '.join(list(map(str,fib_num1))))
