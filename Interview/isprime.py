f7b6d52500342a9b9b3ca44273924ce179496fde
dnootana <30307154+dnootana@users.noreply.github.com>
2025-11-15 18:03:01 +0530

    Create isprime.py

---
+++ Interview/isprime.py
@@ -0,0 +1,18 @@
+def isPrime(n):
+  count = 0
+  for i in range(1, int(math.sqrt(n))+1):
+    if n%i == 0:
+      count += 1 if i*i==n else 2
+      if count > 2:
+        return False
+    return count == 2
+
+def isPrime1(n):
+  import math
+  if n <2:
+    return False
+  for i in range(2, int(math.sqrt(n)+1):
+    if n%i == 0:
+      return False
+    else:
+      return True
